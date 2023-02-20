6d9e90c4b51b5a1aeda230443fae0f494eed1934
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2023-02-20 13:09:51 -0500

    done but not passing all tests

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,24 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map((movie)=>(
+        <div>
+          <h3>Name: {movie.title}</h3>
+          <p>Time: {movie.time} minutes</p>
+          <p>Genres: </p>
+          <ul>
+            {movie.genres.map((genre)=>(
+              <li>{genre}</li>
+            ))}
+          </ul>
+
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Movies;
